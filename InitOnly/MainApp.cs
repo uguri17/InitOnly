1e71b0b646ba4e49a25af4929c5afecd1ad345ce
RND-HW3 <RND-HW3@RND-HW3>
2025-03-31 10:41:49 +0900

    초기 커밋.

---
+++ InitOnly/MainApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InitOnly
+{
+    class Transaction // 거래
+    {
+        public string From { get; init; } // 주는 사람
+        public string To { get; init; } // 받는 사람
+        public int Amount { get; init; } // 거래량
+
+        public override string ToString()
+        {
+            return $"{From} -> {To} : ${Amount}";
+        }
+    }    
+    
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Transaction tr1 = new Transaction { From = "Alice", To = "Bob", Amount = 100 };
+
+            Console.WriteLine(tr1);
+            
+            // tr1.Amount = 30; // !compile error!
+        }
+    }
+}
+// 즉 From, To, Amount는
+// "객체생성"할 때 한번 "초기화(init)" 되면
+// 다음부터는 외부에서 변경이 불가함
